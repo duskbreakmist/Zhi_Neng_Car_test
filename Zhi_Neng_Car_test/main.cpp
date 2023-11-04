2ace30f1f5f8910dc3c35d89ebf70291d434bb28
duskbreakmist <you@example.com>
2023-11-04 21:48:35 +0800

    添加项目文件。

---
+++ Zhi_Neng_Car_test/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <opencv2/core/core.hpp> 
+#include <opencv2/highgui/highgui.hpp>   
+
+using namespace std;
+int main() {
+
+
+
+
+
+	printf(
+		"df\r\n"
+	);
+	return 0;
+}
